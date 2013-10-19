2ede17bc7e79e0ba2aa03ab20a4ef086676618f8
Minna Song <minsong@mit.edu>
2013-10-18 20:33:43 -0700

    Closes Issue #1

--- receiver.cc
+++ receiver.cc
@@ -10,23 +10,15 @@ const int BUFSIZE = 1024;
 
 int main(int argc, char *argv[]) {
 
-      if (argc != 2)
-    {
+   if (argc != 2)
+   {
         std::cerr << "Usage: receiver <port>" << std::endl;
         exit(1);
-      }
-
-    struct sockaddr_in rcvaddr;
-
-    memset(&rcvaddr, 0, sizeof(rcvaddr));
-    rcvaddr.sin_family = AF_INET;
-    rcvaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    rcvaddr.sin_port = htons(atoi(argv[1]));
-    
-    try
-    {
-        Socket sock(UDP);
-        Address addr(rcvaddr);
+   }
+   try
+   {
+        Socket sock(UDP); 
+	Address rcvaddr("127.0.0.1", myatoi(argv[1]));
         sock.bind(rcvaddr);
 
         std::cout << "Waiting for data..." << std::endl;
@@ -37,7 +29,7 @@ int main(int argc, char *argv[]) {
     catch (const Exception & e)
     {
         e.perror();
-	_exit( EXIT_FAILURE);
+	return EXIT_FAILURE;
     }
 
       return 0;
--- sender.cc
+++ sender.cc
@@ -13,22 +13,16 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
    
-    struct sockaddr_in rcvaddr;
-    memset(&rcvaddr, 0, sizeof(rcvaddr));
-    rcvaddr.sin_family = AF_INET;
-    rcvaddr.sin_addr.s_addr = inet_addr(argv[1]);
-    rcvaddr.sin_port = htons(atoi(argv[2]));
-   
     try
     {
-        Socket sock(UDP);
-        Address addr(rcvaddr);
+        Socket sock(UDP); 
+	Address rcvaddr(argv[1],myatoi(argv[2]));
         sock.sendto(rcvaddr, argv[3]);
     }
     catch (const Exception & e)
     {
         e.perror();
-	_exit( EXIT_FAILURE);
+	return EXIT_FAILURE;
     }
 
     return 0;
