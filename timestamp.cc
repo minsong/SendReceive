65d46c8d00f0326a99daf65ce0a19b39e5dc77a5
Minna Song <minsong@mit.edu>
2014-01-15 13:09:08 -0800

    Denormalized timestamp

--- timestamp.cc
+++ timestamp.cc
@@ -5,7 +5,7 @@
 #include "timestamp.hh"
 #include "exception.hh"
 
-static uint64_t timestamp_internal( void )
+uint64_t timestamp( void )
 {
     timespec ts;
     SystemCall( "clock_gettime", clock_gettime( CLOCK_REALTIME, &ts ) );
@@ -14,11 +14,4 @@ static uint64_t timestamp_internal( void )
     millis += uint64_t( ts.tv_sec ) * 1000;
 
     return millis;
-}
-
-uint64_t timestamp( void )
-{
-    static uint64_t first_timestamp = timestamp_internal();
-
-    return timestamp_internal() - first_timestamp;
 }
