9630ad55bd4190f67d9d7a7782dacefcc1bfb7cf
Minna Song <minsong@mit.edu>
2014-01-22 14:21:01 -0800

    Initial commit to open a RemyCC and using its algorithm to send packets.  Testing needed

--- src/client.cc
+++ src/client.cc
@@ -3,54 +3,40 @@
 
 #include "client.hh"
 #include "poller.hh"
-#include "timestamp.hh"
-#include "memory.hh" 
+#include "timestamp.hh" 
 
 using namespace std;
 using namespace PollerShortNames;
 
-static const double alpha = 1.0 / 8.0;
-static const double beta = 1.0 / 4.0;
-
-const uint64_t K = 4;
-
-Client::Client( const string dest_address, const string dest_service  )
-  : addr( dest_address, dest_service, UDP ), sock( UDP )
+Client::Client( WhiskerTree & s_whiskers, const string dest_address, const string dest_service  )
+  : _whiskers( s_whiskers ),
+    _memory(),
+    _track( false ),
+    addr( dest_address, dest_service, UDP ), 
+    sock( UDP )
 {
   cout << "-- New Client --" << endl;
 }
 
 int Client::run( void ){  
 
-  /* Currently using fixed 1s intervals */
-  const int interval_ms = 1000;
-
+  uint64_t largest_ack = -1, the_window = 0, intersend_time = 0,  next_seqnum = 0, flow_id = 0;
   /* Keep track of last time we sent an outgoing datagram */
-  uint64_t last_datagram_sent_ms = timestamp();
-
-  uint64_t ssthresh = numeric_limits<int>::max();
-  uint64_t cwnd = 1;
-  uint64_t ca_incr = 0; //congestion avoidance counter used to increment cwnd once every RTT
-  uint64_t base = 0;
-  uint64_t next_seqnum = 0; 
-
-  /* Initial RTO = 1 second  */
-  uint64_t rto = 1000;
-  uint64_t srtt, rttvar;
+  uint64_t last_datagram_sent = timestamp();
   
+  /* initial window and intersend time */
+  const Whisker & current_whisker( _whiskers.use_whisker( _memory, _track ) );
+  the_window = current_whisker.window( the_window );
+  intersend_time = current_whisker.intersend();
 
- /* Set up the events that we care about */
+  /* Set up the events that we care about */
   Poller poller;
-
   /*Receive a packet */
   poller.add_action( Poller::Action( sock.fd(),
 				     Direction::In,
 				     [&] () {
 				       const auto received_packet = sock.recv();
-				       
-				       update_timeout( received_packet, srtt, rttvar, rto );
-				       update_window_size( ssthresh, ca_incr, cwnd );
-				       base = received_packet.ack_number();
+				       packet_received( received_packet, flow_id, largest_ack, the_window, intersend_time ); 
 
 				       cout << "Received '" << received_packet.payload();
 				       cout << "' with acknum " << received_packet.ack_number();
@@ -61,40 +47,33 @@ int Client::run( void ){
 
   while ( true ) {
     /* Send packets every interval up to available window size */
-    while ( next_seqnum < base + cwnd ) {
-
+    while ( next_seqnum < largest_ack + the_window ) {
+      
       /* Are we due to send an outgoing packet right now? */
       const uint64_t now = timestamp();
-      uint64_t next_packet_is_due = last_datagram_sent_ms + interval_ms;
-
-      if ( now >= next_packet_is_due ) {
-	/* Send a datagram */
+      if ( now >= next_event_time( last_datagram_sent, intersend_time ) ) {
+	//TODO: flow_id set?
 	Packet send_packet( addr , 0, next_seqnum, 0, "Hello from Client" );
 	sock.send( send_packet );
-
-	/* Need to update memory here */
+        _memory.packet_sent( send_packet );
 
 	cout << "Sent packet with seqnum " << send_packet.sequence_number();
 	cout << " at time " << send_packet.send_timestamp();
 	cout << " to " << send_packet.addr().str() << endl; 
 	
-	last_datagram_sent_ms = now;
-	next_packet_is_due = last_datagram_sent_ms + interval_ms;
+	last_datagram_sent = now;
 	next_seqnum++;
       }
     }
 
     /* Wait for an event, and run callback if one comes */
-    auto poll_result = poller.poll( rto );
+    //TODO: How is timer set?
+    auto poll_result = poller.poll( 10000 );
 
     if ( poll_result.result == Poller::Result::Type::Timeout ) {
       cout << "Timed out." << endl;
-      //cout << "Cwnd was: " << cwnd << endl;
-      ssthresh = max( ( uint64_t ) 2, cwnd/2 );
-      cwnd = 1;
-
-      /* On loss, just send next packet */
-      base = next_seqnum;
+      //TODO: What to do on timeout?
+      largest_ack = next_seqnum;
 
     }
     else if ( poll_result.result == Poller::Result::Type::Exit ) {
@@ -107,46 +86,20 @@ int Client::run( void ){
   return EXIT_SUCCESS;
 }
 
-void Client::update_timeout( const Packet &received_packet, uint64_t &srtt, uint64_t &rttvar, uint64_t &rto ){
-  /* Get sample round trip time */
-  const uint64_t received_time = timestamp();
-  const uint64_t r = received_time - received_packet.echo_reply_timestamp();
-  //cout << "Received Time: " << received_time << endl;
-  //cout << "RTT sample: " << r << endl;
-
-  /* Get smoothed round trip time estimate and variance */
-  if ( !(srtt) ) {
-    srtt = r;
-    rttvar = r/2;
-  }
-  else {
-    const uint64_t delta = srtt > r ? srtt-r : r-srtt;
-    rttvar = (1 - beta)*rttvar + beta*delta;
-    srtt = (1-alpha)*srtt + alpha*r;
-  }
-
-  /* Get retransmission timeout */
-  // TODO Clock granularity: rto = srtt + max( G, K*rttvar ); 
-  rto = max( srtt + K*rttvar, ( uint64_t ) 1000 );
-  //cout << "SRTT: " << srtt << " RTTVAR: " << rttvar << endl;
-  //cout << "RTO: " << rto << endl;
+void Client::packet_received( const Packet &packet, const uint64_t &flow_id, uint64_t &largest_ack, uint64_t &the_window, uint64_t &intersend_time ) {
+  /* Assumption: There is no reordering */
+  largest_ack = max( packet.sequence_number(), largest_ack );
+  _memory.packet_received( packet, flow_id );
+  
+  const Whisker & current_whisker( _whiskers.use_whisker( _memory, _track ) );
+  
+  the_window = current_whisker.window( the_window );
+  intersend_time = current_whisker.intersend();
 }
 
-void Client::update_window_size( const uint64_t ssthresh, uint64_t &ca_incr, uint64_t &cwnd ){
-  /* Slow start */ 
-  if (cwnd < ssthresh){
-    cwnd++;
-    cout << "Slow start-> incremented cwnd: " << cwnd << endl;
-  }
-  /* Congestion avoidance: cwnd >= ssthresh */
-  else {
-    /* Count up to cwnd, then increment cwnd */
-    if ( ++ca_incr % cwnd  == 0 ){
-      cwnd++;
-      ca_incr = 0;
-      cout << "Congestion avoidance-> incremented cwnd: " << cwnd << endl;
-    }
-  }
+uint64_t Client::next_event_time( const uint64_t &last_datagram_sent, const uint64_t &intersend_time ) const 
+{
+  return last_datagram_sent + intersend_time;
 }
 
 int main( int argc, char *argv[] ) {
@@ -156,10 +109,21 @@ int main( int argc, char *argv[] ) {
       throw Exception( "client", "Missing argv[ 0 ]" );
     }
     /* Check arguments */
-    if ( argc != 3 ){
-      throw Exception( argv[0] , "DEST_ADDRESS DEST_SERVICE" );
+    if ( argc != 4 ){
+      throw Exception( argv[0] , "FILENAME DEST_ADDRESS DEST_SERVICE" );
     }
-    Client clt( argv[1], argv[2] );
+    WhiskerTree whiskers;
+    string filename( argv[ 1 ] );
+    int fd = open( filename.c_str(), O_RDONLY );
+    if ( fd < 0 ) {
+      throw Exception( argv[0], "Could not open file" );
+    }
+    RemyBuffers::WhiskerTree tree;
+    if ( !tree.ParseFromFileDescriptor( fd ) ) {
+      throw Exception( argv[0], "Could not parse" );
+    }
+    whiskers = WhiskerTree( tree );
+    Client clt( whiskers, argv[2], argv[3] );
     return clt.run();
   } catch ( const Exception & e ) {
     e.perror();
--- src/client.hh
+++ src/client.hh
@@ -3,21 +3,25 @@
 
 #include <string>
 
+#include "whiskertree.hh"
+#include "memory.hh"
 #include "socket.hh"
 
 class Client
 {
 public:
-  Client( const std::string dest_address, const std::string dest_service );
+  Client( WhiskerTree & s_whiskers, const std::string dest_address, const std::string dest_service );
   int run( void );
   
 private:
+  const WhiskerTree & _whiskers;
+  Memory _memory;
+  bool _track;
   Address addr;
   Socket sock;
-  
-  
-  void update_timeout( const Packet &received_packet, uint64_t &srtt, uint64_t &rttvar, uint64_t &rto );
-  void update_window_size( const uint64_t sstresh, uint64_t &ca_incr, uint64_t &cwnd );
+
+  void packet_received( const Packet &packet, const uint64_t &flow_id, uint64_t &largest_ack, uint64_t &the_window, uint64_t &intersend_time );
+  uint64_t next_event_time( const uint64_t &last_datagram_sent, const uint64_t &intersend_time ) const;
 };
 
 #endif /* CLIENT_HH_ */
